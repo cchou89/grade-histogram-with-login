8e25df1a76b5d85fedac8c30cbea04c1bd7230a0
ckc21 <ckc21@sfu.ca>
2020-04-15 03:34:26 -0700

    Add session implementation

--- app/app.js
+++ app/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 
 var session = require('express-session');
 
-var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -22,17 +22,34 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  key: 'session_id',
+  secret: "superduperquadralicious",
+  maxAge: 30*60*1000,  // 30 minutes
+  resave: false,
+  saveUninitialized: true,
+  cookie:{
+    path: '/',
+    expires: 600000
+  }
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+//sanitizes remaining cookies
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -41,41 +58,11 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  // res.status(err.status || 500);
-  // res.render('error');
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.use(session({
-  key: 'username',
-  secret: "superduperquadralicious",
-  maxAge: 30*60*1000,  // 30 minutes
-  resave: false,
-  saveUninitialized: false,
-  cookie:{
-    expires: 600000
-  }
-}));
 
-app.get('/hello', function(req,res){
-  if (req.session.user){
-    // for verified user
-    res.send(req.session.user);
-  } else {
-    // redirect ... back to login
-  }
-
-});
-
-app.get('/setSession', function(req,res){
-  // ... verified user ...
-  databaseQueryUser = {name:"bobby", age:38};
-  req.session.user = databaseQueryUser;
-  // req.session.image = ...;
-  res.send(`
-    <a href="/hello">LINK</a>`);
-});
-
-function generateAccessToken(user){}
 
 
 module.exports = app;
