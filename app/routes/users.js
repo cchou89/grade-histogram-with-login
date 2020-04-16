883beaeea419fcc13d6857466117d3d5979c5922
ckc21 <ckc21@sfu.ca>
2020-04-15 23:23:37 -0700

    implement table display initial commit of histogram

--- app/routes/users.js
+++ app/routes/users.js
@@ -10,14 +10,42 @@ const fs = require('fs');
 router.get('/', verifyToken, function(request, response) {
   console.log(request.session);
   console.log(request.cookies);
-  response.render('users/index', {title: 'CMPT 470 Grade Compiler', session: request.session});
+  response.render('users/index', {
+                                  title: 'CMPT 470 Grade Compiler',
+                                  session: request.session,
+                                  tableHeaders: null,
+                                  json: null});
 });
 
 router.post('/compile', async function(request, response){
   console.log(request.files.csvfile);
   const csvFilePath=request.files.csvfile.tempFilePath;
-  csv().fromFile(csvFilePath)
-      .then()
+  var jsonArray = await csv().fromFile(csvFilePath);
+  var headers = getHeaders(jsonArray);
+  var totals = jsonArray.filter(studentRec => studentRec.studentID === 'total');
+  jsonArray.forEach(function(record) {
+      var semesterGrade = 0;
+      if(record.studentID !== 'total'){
+          headers.forEach(function (item) {
+              if (item !== 'studentID') {
+                  var gradeItem = parseFloat(record[item]);
+                  var totalScore = parseFloat(totals[0][item]);
+                  gradeItem /= 100;
+                  gradeItem *= totalScore;
+                  semesterGrade += gradeItem;
+              }
+          });
+          record.Semester = semesterGrade;
+      }else{
+          record.Semester = 100;
+      }
+  });
+  response.render('users/index', {title: 'CMPT 470 Grade Compiler',
+                                    session: request.session,
+                                    tableHeaders: headers,
+                                    json: jsonArray
+                                    });
+
 });
 
 
@@ -25,6 +53,12 @@ router.get('/:username', verifyToken, function(req, res) {
   res.send('respond with a resource');
 });
 
-
+function getHeaders(jsonArray) {
+    var headers = [];
+    for (x in jsonArray[0]) {
+        headers.push(x);
+    }
+    return headers;
+}
 
 module.exports = router;
